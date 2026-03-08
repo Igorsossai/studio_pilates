dbd39fa71fdd5869468dfd541da448ccf1c0696c
Igorsossai <ilinhaussossai@gmail.com>
2026-03-08 20:15:49 -0300

    urls.py limpo SEM Google

--- core/urls.py
+++ core/urls.py
@@ -1,24 +1,34 @@
 from django.urls import path
 from . import views
 
-app_name = "core"
-
 urlpatterns = [
-    path("pacientes/", views.paciente_list, name="paciente_list"),
-    path("pacientes/novo/", views.paciente_create, name="paciente_create"),
-    path("profissionais/", views.profissional_list, name="profissional_list"),
-    path("profissionais/novo/", views.profissional_create, name="profissional_create"),
-    path("servicos/", views.servico_list, name="servico_list"),
-    path("servicos/novo/", views.servico_create, name="servico_create"),
-    path("atendimentos/", views.atendimento_list, name="atendimento_list"),
-    path("atendimentos/novo/", views.atendimento_create, name="atendimento_create"),
-    path("atendimentos/<int:pk>/realizado/", views.atendimento_realizado, name="atendimento_realizado"),
-    path("comissoes/", views.comissao_list, name="comissao_list"),
-    path("agenda/<str:data>/", views.agenda_dia, name="agenda_dia"),
-    path("agenda/", views.agenda_hoje, name="agenda_hoje"),
-    path("google-connect/", views.google_calendar_connect, name="google_connect"),
-    path("google-callback/", views.google_calendar_callback, name="google_callback"),
-    path("relatorio/", views.relatorio_financeiro, name="relatorio_financeiro"),
+    # HOME
     path('', views.index, name='index'),
 
+    # PACIENTES
+    path('pacientes/', views.paciente_list, name='paciente_list'),
+    path('paciente/novo/', views.paciente_create, name='paciente_create'),
+
+    # PROFISSIONAIS
+    path('profissionais/', views.profissional_list, name='profissional_list'),
+    path('profissional/novo/', views.profissional_create, name='profissional_create'),
+
+    # SERVIÇOS
+    path('servicos/', views.servico_list, name='servico_list'),
+    path('servico/novo/', views.servico_create, name='servico_create'),
+
+    # ATENDIMENTOS
+    path('atendimentos/', views.atendimento_list, name='atendimento_list'),
+    path('atendimento/novo/', views.atendimento_create, name='atendimento_create'),
+
+    # AGENDA
+    path('agenda/', views.agenda_hoje, name='agenda_hoje'),
+    path('agenda/<str:data>/', views.agenda_dia, name='agenda_dia'),
+
+    # RELATÓRIOS
+    path('relatorios/', views.relatorio_list, name='relatorio_list'),
+    path('relatorio-financeiro/', views.relatorio_financeiro, name='relatorio_financeiro'),
+
+    # COMISSÕES
+    path('comissoes/', views.comissao_list, name='comissao_list'),
 ]
